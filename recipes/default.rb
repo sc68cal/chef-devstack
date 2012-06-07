5cc350540946d949bd6e9b1b1c0559b56f7be813
Vishvananda Ishaya <vishvananda@gmail.com>
2012-06-07 11:20:19 -0700

    Force reclone and add SERVICE_PASSWORD

--- recipes/default.rb
+++ recipes/default.rb
@@ -43,4 +43,4 @@ end
 
 execute "killall screen || true"
 
-execute "su -c 'set -e; cd #{node[:devstack][:dir]}/devstack; bash stack.sh > devstack.log' #{node[:devstack][:user]}"
+execute "su -c 'set -e; cd #{node[:devstack][:dir]}/devstack; RECLONE=yes bash stack.sh > devstack.log' #{node[:devstack][:user]}"
