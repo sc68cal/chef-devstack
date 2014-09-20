4144391147f89f452fcc7d6bea7124924991ffad
Sean M. Collins <sean@coreitpro.com>
2014-09-20 01:38:47 -0400

    Update metadata.rb
    Add myself as the maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "Vishvananda Ishaya"
-maintainer_email "vishvananda@gmail.com"
+maintainer       "Sean M. Collins"
+maintainer_email "sean_collins2@cable.comcast.com"
 license          "Apache 2.0"
 description      "Devstack install script"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
