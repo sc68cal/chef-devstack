dac0464ce08f6d7cd675aa5467a852643b61cd79
Sean M. Collins <sean@coreitpro.com>
2015-02-25 11:12:23 -0500

    Remove IP address logic

--- recipes/default.rb
+++ recipes/default.rb
@@ -43,12 +43,4 @@ end
 
 execute "killall screen || true"
 
-eth2_ip = "#{node[:devstack][:floating_range].slice(0..-6)}.2/24"
-
 execute "su -c 'set -e; cd #{node[:devstack][:dir]}/devstack; RECLONE=yes bash stack.sh > devstack.log' #{node[:devstack][:user]}"
-
-execute "add #{node[:devstack][:public_interface]} to bridge" do
-  command "sudo ovs-vsctl add-port --may-exist br-ex #{node[:devstack][:public_interface]}"
-end
-
-execute "sudo ip addr del #{eth2_ip} dev #{node[:devstack][:public_interface]}"
